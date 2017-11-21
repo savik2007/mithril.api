79d1feb2c64a2e9a79c4679f263dcf2c9f24b2f5
PavelVesnin <pavel.vesnin.v@gmail.com>
2017-11-21 17:23:42 +0200

    [patch] - 2FA: set Content-Type header for SMS API

--- lib/mithril_api/otp/sms.ex
+++ lib/mithril_api/otp/sms.ex
@@ -5,9 +5,17 @@ defmodule Mithril.OTP.SMS do
   use Confex, otp_app: :mithril_api
   alias Mithril.ResponseDecoder
 
+  @filter_headers ["content-length", "Content-Length"]
+
   def process_url(url), do: config()[:endpoint] <> url
 
   def process_request_options(options), do: Keyword.merge(config()[:hackney_options], options)
+
+  def process_request_headers(headers) do
+    headers
+    |> Keyword.drop(@filter_headers)
+    |> Kernel.++([{"Content-Type", "application/json"}])
+  end
 
   def send(phone_number, body, type, headers \\ []) do
     "/sms/send"
