28aa79b886bb3ad895a3d2956b6f702c46c8084e
PavelVesnin <pavel.vesnin.v@gmail.com>
2017-12-26 17:27:47 +0200

    [patch] - revert command for DB migration

--- bin/hooks/pre-start.sh
+++ bin/hooks/pre-start.sh
@@ -4,7 +4,7 @@ APP_NAME="mithril_api"
 
 if [ "${DB_MIGRATE}" == "true" ]; then
   echo "[WARNING] Migrating database!"
-  ./bin/$APP_NAME command "Mithril.ReleaseTasks" migrate!
+  ./bin/$APP_NAME command "Elixir.Mithril.ReleaseTasks" migrate!
 fi;
 
 if [ "${LOAD_FIXTURES}" == "true" ]; then
