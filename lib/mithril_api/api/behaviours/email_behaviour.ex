73829296dd264dba1ec68fd170edc9a0d935b38f
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-03-30 17:44:46 +0300

    [patch] - removed unused Email API

--- lib/mithril_api/api/behaviours/email_behaviour.ex
+++
@@ -1,7 +0,0 @@
-defmodule Mithril.API.EmailBehaviour do
-  @moduledoc false
-
-  @callback send(email :: binary, verification_code :: binary) ::
-              {:ok, result :: term}
-              | {:error, reason :: term}
-end
