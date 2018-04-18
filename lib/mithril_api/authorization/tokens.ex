c775f823c63762a25d3c7971b6c5bf9f01a6dacd
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-04-18 17:53:28 +0300

    [minor] - modules reorganization #3 BrokerScope

--- lib/mithril_api/authorization/tokens.ex
+++ lib/mithril_api/authorization/tokens.ex
@@ -13,9 +13,7 @@ defmodule Mithril.Authorization.Tokens do
   alias Mithril.Authentication
   alias Mithril.Authentication.Factor
   alias Mithril.Authorization.GrantType.{Password, AccessToken2FA}
-
-  @direct ClientAPI.access_type(:direct)
-  @broker ClientAPI.access_type(:broker)
+  alias Mithril.Authorization.BrokerScope
 
   @refresh_token "refresh_token"
   @access_token "access_token"
@@ -26,7 +24,6 @@ defmodule Mithril.Authorization.Tokens do
   @approve_factor "APPROVE_FACTOR"
   @type_field "request_authentication_factor_type"
   @factor_field "request_authentication_factor"
-
 
   @doc """
     Create new access_tokens based on grant_type the request came with
@@ -202,7 +199,7 @@ defmodule Mithril.Authorization.Tokens do
          :ok <- check_client_is_blocked(client),
          user <- UserAPI.get_user!(token.user_id),
          :ok <- check_user_is_blocked(user),
-         {:ok, token} <- put_broker_scopes(token, client, api_key) do
+         {:ok, token} <- BrokerScope.put_broker_scope_into_token_details(token, client, api_key) do
       {:ok, token}
     else
       {:error, _} = err -> err
@@ -212,56 +209,5 @@ defmodule Mithril.Authorization.Tokens do
 
   def expired?(%Token{} = token) do
     token.expires_at <= :os.system_time(:seconds)
-  end
-
-  defp put_broker_scopes(token, client, api_key) do
-    case Map.get(client.priv_settings, "access_type") do
-      nil ->
-        Error.access_denied("Client settings must contain access_type.")
-
-      # Clients such as NHS Admin, MIS
-      @direct ->
-        {:ok, token}
-
-      # Clients such as MSP, PHARMACY
-      @broker ->
-        api_key
-        |> validate_api_key()
-        |> fetch_client_by_secret()
-        |> fetch_broker_scope()
-        |> put_broker_scope_into_token_details(token)
-    end
-  end
-
-  defp validate_api_key(api_key) when is_binary(api_key), do: api_key
-  defp validate_api_key(_), do: Error.invalid_request("API-KEY header required.")
-
-  defp fetch_client_by_secret({:error, _} = err), do: err
-
-  defp fetch_client_by_secret(api_key) do
-    case ClientAPI.get_client_by(secret: api_key) do
-      %ClientAPI.Client{} = client ->
-        client
-
-      _ ->
-        Error.invalid_request("API-KEY header is invalid.")
-    end
-  end
-
-  defp fetch_broker_scope({:error, _} = err), do: err
-
-  defp fetch_broker_scope(%ClientAPI.Client{priv_settings: %{"broker_scope" => broker_scope}}) do
-    broker_scope
-  end
-
-  defp fetch_broker_scope(_) do
-    Error.invalid_request("Incorrect broker settings.")
-  end
-
-  defp put_broker_scope_into_token_details({:error, _} = err, _token), do: err
-
-  defp put_broker_scope_into_token_details(broker_scope, token) do
-    details = Map.put(token.details, "broker_scope", broker_scope)
-    {:ok, Map.put(token, :details, details)}
   end
 end
