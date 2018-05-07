b63ef2829612730ad23260a692e98cad358507ed
Alex Kovalevych <alexkovalevych@gmail.com>
2018-05-07 14:25:52 +0300

    [minor] add tokens and users indexes

---
+++ priv/repo/migrations/20180507111406_add_token_name_expires_at_index.exs
@@ -0,0 +1,11 @@
+defmodule Mithril.Repo.Migrations.AddTokenNameExpiresAtIndex do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  @disable_ddl_transaction true
+
+  def change do
+    create(index(:tokens, [:name, :user_id, :expires_at], concurrently: true))
+  end
+end
