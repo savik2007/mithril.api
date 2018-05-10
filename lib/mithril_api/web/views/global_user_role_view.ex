402bce9d50fc718f35e99765f7f14bb81e1b67bc
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-05-10 15:02:18 +0300

    [patch] - global role view refactoring

--- lib/mithril_api/web/views/global_user_role_view.ex
+++ lib/mithril_api/web/views/global_user_role_view.ex
@@ -1,21 +1,15 @@
 defmodule Mithril.Web.GlobalUserRoleView do
   use Mithril.Web, :view
-  alias Mithril.Web.UserRoleView
-  alias Mithril.RoleAPI.Role
+
+  @fields ~w(id user_id role_id inserted_at updated_at)a
 
   def render("show.json", %{global_user_role: global_user_role}) do
     render_one(global_user_role, __MODULE__, "global_user_role.json")
   end
 
-  def render("global_user_role.json", %{global_user_role: global_user_role}) do
-    %{
-      id: global_user_role.id,
-      user_id: global_user_role.user_id,
-      role_id: global_user_role.role_id,
-      role_name: global_user_role.role.name,
-      created_at: global_user_role.inserted_at,
-      updated_at: global_user_role.updated_at,
-      scope: global_user_role.role.scope
-    }
+  def render("global_user_role.json", %{global_user_role: %{role: role} = global_user_role}) do
+    global_user_role
+    |> Map.take(@fields)
+    |> Map.merge(%{role_name: role.name, scope: role.scope})
   end
 end
