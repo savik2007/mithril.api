bf074681c020b8d3925b5992dafb6eba88862402
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-09-06 19:04:58 +0300

    fixed authorization tests

--- test/unit/authorization/grant_type_auth_code_test.exs
+++ test/unit/authorization/grant_type_auth_code_test.exs
@@ -1,74 +1,89 @@
 defmodule Mithril.Authorization.GrantType.AuthorizationCodeTest do
   use Mithril.DataCase, async: true
 
+  alias Ecto.Changeset
+  alias Mithril.TokenAPI
   alias Mithril.Authorization.GrantType.AuthorizationCode, as: AuthorizationCodeGrantType
 
   test "creates code-granted access token" do
     client = insert(:client)
-    user = insert(:user)
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
+    insert(:connection, client: client)
+    insert(:connection, client: client)
+    redirect_uri = "https://example.com/redirect_uri"
+    connection = insert(:connection, client: client, redirect_uri: redirect_uri)
+
+    insert(:app,
+      user: client.user,
+      client: client,
       scope: "legal_entity:read legal_entity:write"
-    })
+    )
 
-    code_grant = create_code_grant_token(client, user, "legal_entity:read")
+    code_grant = create_code_grant_token(connection, client.user, "legal_entity:read")
 
     {:ok, token} =
       AuthorizationCodeGrantType.authorize(%{
         "client_id" => client.id,
-        "client_secret" => client.secret,
+        "client_secret" => connection.secret,
         "code" => code_grant.value,
-        "redirect_uri" => client.redirect_uri <> "?world"
+        "redirect_uri" => redirect_uri <> "?world"
       })
 
     assert token.name == "access_token"
     assert token.value
     assert token.expires_at
-    assert token.user_id == user.id
+    assert token.user_id == client.user.id
     assert token.details.client_id == client.id
     assert token.details.refresh_token
     assert token.details.grant_type == "authorization_code"
-    assert token.details.redirect_uri == client.redirect_uri
+    assert token.details.redirect_uri == redirect_uri
     assert token.details.scope == "legal_entity:read"
 
-    assert true = Mithril.TokenAPI.get_token!(code_grant.id).details["used"]
+    assert true = TokenAPI.get_token!(code_grant.id).details["used"]
   end
 
-  test "it returns Request must include at least... error" do
-    message = "Request must include at least client_id, client_secret, code and redirect_uri parameters."
-
-    assert {:error, {:unprocessable_entity, ^message}} =
-             AuthorizationCodeGrantType.authorize(%{
-               "scope" => "legal_entity:read"
-             })
-  end
-
-  test "it returns invalid client id or secret error" do
+  test "it returns invalid client id or secret error on invalid client_id" do
     client = insert(:client)
+    connection = insert(:connection, client: client)
 
     message = "Invalid client id or secret."
 
     assert {:error, {:access_denied, %{message: ^message, type: "invalid_client"}}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => "F75029D0-DDBA-4897-A6F2-9A785222FD67",
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => "some_code",
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
+               "scope" => "legal_entity:read"
+             })
+  end
+
+  test "it returns invalid client id or secret error on invalid secret" do
+    client = insert(:client)
+    connection = insert(:connection, client: client)
+
+    message = "Invalid client id or secret."
+
+    assert {:error, {:access_denied, %{message: ^message, type: "invalid_client"}}} =
+             AuthorizationCodeGrantType.authorize(%{
+               "client_id" => client.id,
+               "client_secret" => "invalid secret",
+               "code" => "some_code",
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
   end
 
   test "it returns Token Not Found error" do
     client = insert(:client)
+    connection = insert(:connection, client: client)
 
     assert {:error, {:access_denied, errors}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => "some_code",
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
 
@@ -77,133 +92,136 @@ defmodule Mithril.Authorization.GrantType.AuthorizationCodeTest do
 
   test "it returns Resource owner revoked access for the client error" do
     client = insert(:client)
-    user = insert(:user)
-
-    code_grant = create_code_grant_token(client, user)
+    connection = insert(:connection, client: client)
+    code_grant = create_code_grant_token(connection, client.user)
 
     message = "Resource owner revoked access for the client."
 
     assert {:error, {:access_denied, ^message}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => code_grant.value,
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
   end
 
   test "it returns redirection URI client error" do
     client = insert(:client)
-    user = insert(:user)
+    connection = insert(:connection, client: client)
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
+    insert(:app,
+      user: client.user,
+      client: client,
       scope: "legal_entity:read legal_entity:write"
-    })
+    )
 
-    code_grant = create_code_grant_token(client, user)
+    code_grant = create_code_grant_token(connection, client.user)
 
     message = "The redirection URI provided does not match a pre-registered value."
 
     assert {:error, {:access_denied, %{message: ^message}}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => code_grant.value,
-               "redirect_uri" => "some_suspicios_uri",
+               "redirect_uri" => "some_suspicious_uri",
                "scope" => "legal_entity:read"
              })
   end
 
   test "it returns token expired error" do
     client = insert(:client)
-    user = insert(:user)
+    connection = insert(:connection, client: client)
     scope = "legal_entity:read legal_entity:write"
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
+    insert(:app,
+      user: client.user,
+      client: client,
       scope: scope
-    })
+    )
 
-    code_grant = create_code_grant_token(client, user, scope, 0)
+    code_grant = create_code_grant_token(connection, client.user, scope, 0)
 
     message = "Token expired."
 
     assert {:error, {:access_denied, %{message: ^message}}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => code_grant.value,
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
   end
 
   test "it returns token not found or expired error" do
     client = insert(:client)
-    user = insert(:user)
+    connection = insert(:connection, client: client)
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
+    insert(:app,
+      user: client.user,
+      client: client,
       scope: "legal_entity:read legal_entity:write"
-    })
+    )
 
     client2 = insert(:client, name: "Another name")
-    code_grant = create_code_grant_token(client2, user)
+    connection2 = insert(:connection, client: client2)
+    code_grant = create_code_grant_token(connection2, client2.user)
 
     message = "Token not found or expired."
 
     assert {:error, {:access_denied, %{message: ^message, type: "invalid_grant"}}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => code_grant.value,
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
   end
 
   test "it returns token was used error" do
     client = insert(:client)
-    user = insert(:user)
+    connection = insert(:connection, client: client)
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
+    insert(:app,
+      user: client.user,
+      client: client,
       scope: "legal_entity:read legal_entity:write"
-    })
+    )
 
-    code_grant = create_code_grant_token(client, user)
+    code_grant = create_code_grant_token(connection, client.user)
 
-    {:ok, code_grant} =
-      Mithril.TokenAPI.update_token(code_grant, %{details: Map.put_new(code_grant.details, :used, true)})
+    {:ok, code_grant} = TokenAPI.update_token(code_grant, %{details: Map.put_new(code_grant.details, :used, true)})
 
     message = "Token has already been used."
 
     assert {:error, {:access_denied, ^message}} =
              AuthorizationCodeGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "code" => code_grant.value,
-               "redirect_uri" => client.redirect_uri,
+               "redirect_uri" => connection.redirect_uri,
                "scope" => "legal_entity:read"
              })
   end
 
-  test "it returns error on missing values" do
-    message = "Request must include at least client_id, client_secret, code and redirect_uri parameters."
+  describe "invalid params" do
+    test "empty field values" do
+      assert %Changeset{valid?: false} =
+               AuthorizationCodeGrantType.authorize(%{
+                 "client_id" => nil,
+                 "client_secret" => nil,
+                 "code" => nil,
+                 "redirect_uri" => nil,
+                 "scope" => nil
+               })
+    end
 
-    assert {:error, {:unprocessable_entity, ^message}} =
-             AuthorizationCodeGrantType.authorize(%{
-               "client_id" => nil,
-               "client_secret" => nil,
-               "code" => nil,
-               "redirect_uri" => nil,
-               "scope" => nil
-             })
+    test "missed required fields" do
+      assert %Changeset{valid?: false} = AuthorizationCodeGrantType.authorize(%{"scope" => "legal_entity:read"})
+    end
   end
 end
--- test/unit/authorization/grant_type_refresh_token_test.exs
+++ test/unit/authorization/grant_type_refresh_token_test.exs
@@ -1,73 +1,101 @@
 defmodule Mithril.Authorization.GrantType.RefreshTokenTest do
   use Mithril.DataCase, async: true
 
+  alias Ecto.Changeset
   alias Mithril.Authorization.GrantType.RefreshToken, as: RefreshTokenGrantType
 
-  test "creates refresh-granted access token" do
-    client = insert(:client)
-    user = insert(:user)
+  describe "create refresh-granted access token" do
+    setup do
+      client = insert(:client)
+      connection = insert(:connection, client: client)
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
-      scope: "legal_entity:read legal_entity:write"
-    })
+      insert(:app,
+        user: client.user,
+        client: client,
+        scope: "legal_entity:read legal_entity:write"
+      )
 
-    refresh_token = create_refresh_token(client, user)
+      {:ok, %{client: client, user: client.user, connection: connection}}
+    end
 
-    assert refresh_token.name == "refresh_token"
-    assert refresh_token.details.scope == ""
+    test "success", %{client: client, user: user, connection: connection} do
+      refresh_token = create_refresh_token(client, user)
 
-    {:ok, token} =
-      RefreshTokenGrantType.authorize(%{
-        "client_id" => client.id,
-        "client_secret" => client.secret,
-        "refresh_token" => refresh_token.value
-      })
+      assert refresh_token.name == "refresh_token"
+      assert refresh_token.details.scope == ""
 
-    assert token.name == "access_token"
-    assert token.value
-    assert token.expires_at
-    assert token.user_id == user.id
-    assert token.details.client_id == client.id
-    assert token.details.grant_type == "refresh_token"
-    assert token.details.scope == "legal_entity:read legal_entity:write"
-  end
+      {:ok, token} =
+        RefreshTokenGrantType.authorize(%{
+          "client_id" => client.id,
+          "client_secret" => connection.secret,
+          "refresh_token" => refresh_token.value
+        })
 
-  test "it returns Request must include at least... error" do
-    message = "Request must include at least client_id, client_secret and refresh_token parameters."
-    assert {:error, {:unprocessable_entity, ^message}} = RefreshTokenGrantType.authorize(%{})
+      assert token.name == "access_token"
+      assert token.value
+      assert token.expires_at
+      assert token.user_id == user.id
+      assert token.details.client_id == client.id
+      assert token.details.grant_type == "refresh_token"
+      assert token.details.scope == "legal_entity:read legal_entity:write"
+    end
+
+    test "it returns token expired error", %{client: client, user: user, connection: connection} do
+      refresh_token = create_refresh_token(client, user, 0)
+
+      assert {:error, {:access_denied, %{message: "Token expired.", type: "token_expired"}}} =
+               RefreshTokenGrantType.authorize(%{
+                 "client_id" => client.id,
+                 "client_secret" => connection.secret,
+                 "refresh_token" => refresh_token.value
+               })
+    end
+
+    test "it returns token not found or expired error", %{client: client, user: user, connection: connection} do
+      client2 = insert(:client, name: "Another name")
+      refresh_token = create_refresh_token(client2, user)
+
+      assert {:error, {:access_denied, %{type: "invalid_grant"}}} =
+               RefreshTokenGrantType.authorize(%{
+                 "client_id" => client.id,
+                 "client_secret" => connection.secret,
+                 "refresh_token" => refresh_token.value
+               })
+    end
   end
 
   test "it returns invalid client id or secret error" do
     client = insert(:client)
+    connection = insert(:connection, client: client)
 
     message = "Invalid client id or secret."
 
     assert {:error, {:access_denied, %{message: ^message}}} =
              RefreshTokenGrantType.authorize(%{
                "client_id" => "F75029D0-DDBA-4897-A6F2-9A785222FD67",
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "refresh_token" => "some_value"
              })
   end
 
   test "it returns Token Not Found error" do
     client = insert(:client)
+    connection = insert(:connection, client: client)
 
     assert {:error, {:access_denied, errors}} =
              RefreshTokenGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "refresh_token" => "some_token"
              })
 
-    assert %{message: "Token not found.", type: "invalid_grant"} = errors
+    assert %{message: "Token not found.", type: "token_not_found"} == errors
   end
 
   test "it returns Resource owner revoked access for the client error" do
-    client = insert(:client)
     user = insert(:user)
+    client = insert(:client)
+    connection = insert(:connection, client: client)
 
     refresh_token = create_refresh_token(client, user)
 
@@ -76,60 +104,23 @@ defmodule Mithril.Authorization.GrantType.RefreshTokenTest do
     assert {:error, {:access_denied, ^message}} =
              RefreshTokenGrantType.authorize(%{
                "client_id" => client.id,
-               "client_secret" => client.secret,
+               "client_secret" => connection.secret,
                "refresh_token" => refresh_token.value
              })
   end
 
-  test "it returns token expired error" do
-    client = insert(:client)
-    user = insert(:user)
+  describe "invalid params" do
+    test "empty field values" do
+      %Changeset{valid?: false} =
+        RefreshTokenGrantType.authorize(%{
+          "client_id" => nil,
+          "client_secret" => nil,
+          "refresh_token" => nil
+        })
+    end
 
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
-      scope: "legal_entity:read legal_entity:write"
-    })
-
-    refresh_token = create_refresh_token(client, user, 0)
-
-    assert {:error, {:access_denied, %{message: "Token expired.", type: "invalid_grant"}}} =
-             RefreshTokenGrantType.authorize(%{
-               "client_id" => client.id,
-               "client_secret" => client.secret,
-               "refresh_token" => refresh_token.value
-             })
-  end
-
-  test "it returns token not found or expired error" do
-    client = insert(:client)
-    user = insert(:user)
-
-    Mithril.AppAPI.create_app(%{
-      user_id: user.id,
-      client_id: client.id,
-      scope: "legal_entity:read legal_entity:write"
-    })
-
-    client2 = insert(:client, name: "Another name")
-    refresh_token = create_refresh_token(client2, user)
-
-    assert {:error, {:access_denied, %{type: "invalid_grant"}}} =
-             RefreshTokenGrantType.authorize(%{
-               "client_id" => client.id,
-               "client_secret" => client.secret,
-               "refresh_token" => refresh_token.value
-             })
-  end
-
-  test "it returns error on missing values" do
-    message = "Request must include at least client_id, client_secret and refresh_token parameters."
-
-    {:error, {:unprocessable_entity, ^message}} =
-      RefreshTokenGrantType.authorize(%{
-        "client_id" => nil,
-        "client_secret" => nil,
-        "refresh_token" => nil
-      })
+    test "missed required fields" do
+      assert %Changeset{valid?: false} = RefreshTokenGrantType.authorize(%{})
+    end
   end
 end
