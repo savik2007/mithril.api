c6cae236d292207a4c3588bcfa127cb6516d414d
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-09-07 13:21:04 +0300

    fixed user_api test

--- test/acceptance/cabinet_test.exs
+++ test/acceptance/cabinet_test.exs
@@ -1,6 +1,5 @@
 defmodule Mithril.Acceptance.CabinetTest do
   use Mithril.Web.ConnCase
-  alias Mithril.Clients.Client
 
   describe "create approval for MIS client that doesn't have User.user_roles with MIS client_id" do
     setup %{conn: conn} do
