d0e61459546d386add3d7e30b5d946329c14d185
IrinaRevutska <iren.revutska@gmail.com>
2019-02-14 00:03:28 +0200

    device, medication_statement, risk_assessment scopes

--- apps/core/priv/repo/migrations/20181108162351_add_medical_events_scopes.exs
+++ apps/core/priv/repo/migrations/20181108162351_add_medical_events_scopes.exs
@@ -7,7 +7,7 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
     DO
     $$DECLARE r record;
     BEGIN
-        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel']) as scope
+        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel', 'risk_assessment:read', 'device:read', 'medication_statement:read']) as scope
     LOOP
         EXECUTE 'UPDATE client_types SET scope = lower(replace(upper(scope), upper($1), '''')) WHERE upper(scope) ~ upper($2);' USING r.scope, r.scope;
     END LOOP;
@@ -16,9 +16,9 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
 
     execute("""
     UPDATE client_types
-    SET scope = trim(scope) || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel'
+    SET scope = trim(scope) || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read'
     WHERE name IN ('MSP')
-    AND scope NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel%';
+    AND scope NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read%';
     """)
 
     # ======== Update roles ===========
@@ -26,7 +26,7 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
     DO
     $$DECLARE r record;
     BEGIN
-        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel']) as scope
+        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel', 'risk_assessment:read', 'device:read', 'medication_statement:read']) as scope
     LOOP
         EXECUTE 'UPDATE roles SET scope = lower(replace(upper(scope), upper($1), '''')) WHERE upper(scope) ~ upper($2);' USING r.scope, r.scope;
     END LOOP;
@@ -35,9 +35,9 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
 
     execute("""
     UPDATE roles
-    SET scope = trim(scope) || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel'
+    SET scope = trim(scope) || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read'
     WHERE name IN ('DOCTOR')
-    AND scope NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel%';
+    AND scope NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read , 'risk_assessment:read', 'device:read', 'medication_statement:read%';
     """)
 
     # ======== Update broker scopes ===========
@@ -45,7 +45,7 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
     DO
     $$DECLARE r record;
     BEGIN
-        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel']) as scope
+        FOR r IN SELECT unnest(ARRAY['encounter:write', 'encounter:read', 'episode:write', 'episode:read', 'job:read', 'condition:read', 'observation:read', 'immunization:read', 'allergy_intolerance:read', 'encounter:cancel  risk_assessment:read device:read medication_statement:read']) as scope
     LOOP
         EXECUTE 'UPDATE clients SET priv_settings = jsonb_set(priv_settings, ''{broker_scope}'', to_json(lower(replace(upper(trim(priv_settings ->> ''broker_scope'')), upper($1), '''')))::jsonb) WHERE upper(priv_settings::text) ~ upper($2);' USING r.scope, r.scope;
     END LOOP;
@@ -54,10 +54,10 @@ defmodule Core.Repo.Migrations.AddMedicalEventsScopes do
 
     execute("""
     UPDATE clients c
-        SET priv_settings = jsonb_set(priv_settings, '{broker_scope}', to_json(trim(priv_settings ->> 'broker_scope') || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel')::jsonb)
+        SET priv_settings = jsonb_set(priv_settings, '{broker_scope}', to_json(trim(priv_settings ->> 'broker_scope') || ' encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read')::jsonb)
       WHERE c.client_type_id = (SELECT client_types.id
                     FROM client_types WHERE client_types.name = 'MIS')
-        AND c.priv_settings ->> 'broker_scope' NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel%';
+        AND c.priv_settings ->> 'broker_scope' NOT LIKE '%encounter:write encounter:read episode:write episode:read job:read condition:read observation:read immunization:read allergy_intolerance:read encounter:cancel risk_assessment:read device:read medication_statement:read%';
     """)
   end
 end
